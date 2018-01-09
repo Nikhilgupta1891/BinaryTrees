14a12ae7ad3e5b5be2802b1c2d8e9a1c887298a5
Nikhil Gupta <NG043556@cerner.net>
2018-01-09 09:24:31 -0600

    Remove ignored files

--- BinaryTrees/src/Node.java
+++ BinaryTrees/src/Node.java
@@ -2,50 +2,51 @@ class Node {
 
 	int data;
 	Node left, right;
+
 	public Node(int data) {
 		this.data = data;
 	}
-	
+
 	public void insert(int value) {
-		if(value<=data) {
-			if(left == null) {
+		if (value <= data) {
+			if (left == null) {
 				left = new Node(value);
 			} else {
 				left.insert(value);
 			}
 		} else {
-			if(right ==null) {
+			if (right == null) {
 				right = new Node(value);
 			} else {
 				right.insert(value);
 			}
 		}
 	}
-	
+
 	public boolean contains(int value) {
-		if(value == data) {
+		if (value == data) {
 			return true;
-		} else if(value < data) {
-			if(left == null) {
+		} else if (value < data) {
+			if (left == null) {
 				return false;
 			} else {
 				return left.contains(value);
 			}
 		} else {
-			if(right == null) {
+			if (right == null) {
 				return false;
 			} else {
 				return right.contains(value);
 			}
 		}
 	}
-	
+
 	public void inOrderTraversal() {
-		if(left!=null) {
+		if (left != null) {
 			left.inOrderTraversal();
-		} 
+		}
 		System.out.println(data);
-		if(right!=null) {
+		if (right != null) {
 			right.inOrderTraversal();
 		}
 	}
